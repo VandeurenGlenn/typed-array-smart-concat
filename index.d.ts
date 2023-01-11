ca304ee8935bef6a56ebdace87be71ac346f72b6
vandeurenglenn <vandeurenglenn@gmail.com>
2023-01-11 19:45:23 +0100

    Auto declare

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (input: any, prefix: any) => Uint8Array;
+export default _default;
---
+++ index.js
@@ -0,0 +1,28 @@
+import varint from 'varint';
+
+var index = (input, prefix) => {
+    const encodedArray = [];
+    const length = input.reduce((total, current) => {
+        const encoded = varint.encode(current.length);
+        encodedArray.push(encoded);
+        total += current.length + encoded.length;
+        return total;
+    }, 0);
+    const typedArray = new Uint8Array(prefix ? prefix.length + length : length);
+    let currentIndex = 0;
+    let index = 0;
+    if (prefix) {
+        typedArray.set(prefix);
+        currentIndex += prefix.length;
+    }
+    for (const source of input) {
+        typedArray.set(encodedArray[index], currentIndex);
+        currentIndex += encodedArray[index].length;
+        typedArray.set(source, currentIndex);
+        currentIndex += source.length;
+        index += 1;
+    }
+    return typedArray;
+};
+
+export { index as default };
